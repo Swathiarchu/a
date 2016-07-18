2a01654133b7e5db2d159e8306ee112611f4c430
Swathi.S <swathisiva611@gmail.com>
2016-07-18 14:38:21 +0530

    Create great.c

---
+++ great.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char c,d;
+clrscr();
+	scanf("%c %c",&c,&d);
+	if(a>b)
+		printf("%c c is greatest\n",c);
+	else if(b>a)
+		printf("%c d is greatest\n",d);
+	else
+		printf("\nBoth are equal");
+		getch();
+}
